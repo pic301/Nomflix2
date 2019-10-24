bdd8efc41c8e8c724da77249dd1719a5df7038ff
Hanzo <>
2019-10-24 21:45:30 +0900

    no message

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,14 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Section from 'Components/Section'
+import Loader from '../../Components/Loader'
 
 const Container= styled.div `
 padding: 0px 10px ;
 `;
 
 const HomePresenter = ({nowPlaying, upcoming, popular, loading, error }) =>
-loading ? null:
-  (<Container>
+loading ? (
+    <Loader/>
+  ):(
+  <Container>
   {nowPlaying && nowPlaying.length > 0 && 
     <Section title="Now Playing">
     {nowPlaying.map(movie =>movie.title)}</Section>}
