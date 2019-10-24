4edf9e09b8cb3a75acf3502ee53b485f7cb3c814
Hanzo <>
2019-10-24 22:05:34 +0900

    no message

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -15,15 +15,15 @@ loading ? (
   <Container>
   {nowPlaying && nowPlaying.length > 0 && 
     <Section title="Now Playing">
-    {nowPlaying.map(movie =>movie.title)}</Section>}
+    {nowPlaying.map(movie =><span key={movie.id}>{movie.title}</span>)}</Section>}
 
      {upcoming && upcoming.length > 0 && 
     <Section title="Upcoming Movies">
-    {upcoming.map(movie =>movie.title)}</Section>}
+    {upcoming.map(movie =><span key={movie.id}>{movie.title}</span>)}</Section>}
     
     {popular && popular.length > 0 && 
     <Section title="Popular Movies">
-    {popular.map(movie =>movie.title)}</Section>}
+    {popular.map(movie =><span key={movie.id}>{movie.title}</span>)}</Section>}
 
   
 </Container>)
