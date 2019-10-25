b92b274d1d31b62765fbc8cd4a7b18b0bb085682
Hanzo <>
2019-10-25 09:56:47 +0900

    no message

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,15 +1,63 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from 'Components/Loader'
+import Section from 'Components/Section'
+
+const Container = styled.div`
+padding: 0px 20px;
+`;
+
+const Form = styled.form`
+margin-bottom:50px;
+width:100%;
+`;
+
+const Input = styled.input`
+all:unset;
+font-size: 28px;
+width:100%;
+`;
+
+//props로 추가하면 여기서써야겠지
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  loading,
+  searchTerm,
+  handleSubmit,
+  error,
+  updateTerm,
+}) => <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder='Search Movies or TV Show..'
+        value={searchTerm}
+        onChange={updateTerm} />
+    </Form>
+    {loading ?
+      (<Loader />)
+      :
+      <>
+        {movieResults && movieResults.length > 0 &&
+          <Section title='Movie Results'>
+            {movieResults.map(movie => (
+              <span key={movie.id}>{movie.title} </span>
+            ))}
+          </Section>}
+          {tvResults && tvResults.length > 0 &&
+          <Section title='Movie Results'>
+            {tvResults.map(show => (
+              <span key={show.id}>{show.name} </span>
+            ))}
+          </Section>}
+      </>}
+  </Container>
+//로더는 로딩이 멈췄을때 무비리절트로 갈꺼고 렝스의 길이가 0이상이면 섹션타이틀로가고
 
 
-const SearchPresenter = ({   movieResults,
-    tvResults,
-    loading,
-    searchTerm,
-    handleSubmit,
-    error
-  }) => null;
+
+
 
 SearchPresenter.propTypes = {
   movieResults: PropTypes.array,
@@ -17,7 +65,8 @@ SearchPresenter.propTypes = {
   error: PropTypes.string,
   loading: PropTypes.bool.isRequired,
   searchTerm: PropTypes.string,
-  handleSubmit: PropTypes.func.isRequired
+  handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired
 };
 
 export default SearchPresenter;
